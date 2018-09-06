401ff57536afa8a9da86b441b082442f82f3b0af
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-09-05 22:33:31 -0300

    dsds

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const qs = require('querystring')
-
+const { createEventAdapter } = require('@slack/events-api')
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET)
 const app = express()
 const port = process.env.PORT || 3000
-const gaKey = process.env.GA_KEY || 'UA-101595764-2'
-
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 
 app.get('/', (req, res) => {
     res.send("i'm alive!")
 })
 
-app.get('/event', (req, res) => {
-    console.log(req.body)
+app.use('/slack/events', slackEvents.expressMiddleware())
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({
-        "token": "Jhj5dZrVaK7ZwHHjRyZWjbDl",
-        "challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
-        "type": "url_verification"
-    }))
+slackEvents.on('message', e => {
+    console.log(e)
+    // console.log(`Received a message event: user ${e.user} in channel ${e.channel} says ${e.text}`)
 })
+
+slackEvents.on('error', console.error)
 
 app.listen(port, () => console.info(`Listening on port ${port}`))
