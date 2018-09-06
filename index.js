51f533496a4c2d0a182241ac5462f1c34bb1275b
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-09-05 22:21:44 -0300

    sr

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const qs = require('querystring')
+
+const app = express()
+const port = process.env.PORT || 3000
+const gaKey = process.env.GA_KEY || 'UA-101595764-2'
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+
+app.get('/', (req, res) => {
+    res.send("i'm alive!")
+})
+
+app.get('/event', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(port, () => console.info(`Listening on port ${port}`))
