6c95694b45d2bec0a92ccdcbf76ece68a6102892
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-09-13 16:11:13 -0300

    stable

--- index.js
+++ index.js
@@ -10,59 +10,35 @@ const app = express()
 app.get('/', (req, res) => res.send("i'm alive!"))
 app.use('/slack/events', slackEvents.expressMiddleware())
 
-slackEvents.on('message', e => {
+const handleEvent = e => {
     console.log(e)
     let params = {
         v: 1,
-		tid: ga,
+		tid: process.env.GA || 'UA-101595764-3',
         cid: e.user,
         cd1: e.user,
 		cd2: e.channel,
-        cd3: '',
-        cd4: e.reaction,
+        cd3: e.thread_ts,
+        cd4: e.type,
         ds: "slack",
         cs: "slack",
         dh: "https://temdelivery.slack.com",
-        dp:	`/${e.channel}`,
-		dt:	`Slack Channel: ${e.channel}`,
+        dp:	`/${e.type==="message" ? e.channel : e.item.channel}`,
+		dt:	`Slack Channel: ${e.type==="message" ? e.channel : e.item.channel}`,
 		t: 	"event",
-        ec: `message in ${e.channel} >> ${e.item.ts}`,
-        ea: `by ${e.user} in ${e.item.channel} | ${e.thread_ts ? 'is a thread reply' : ''}`,
-        el: e.text,
+        ec: e.type==="message" ? e.channel : e.item.channel,
+        ea: `${e.user}`,
+        el: e.type==="message" ? e.text : e.reaction,
         ev: 1
     }
     request.post(`https://www.google-analytics.com/collect?${qs.stringify(params)}`, (error, response, body) => {
         console.info(error)
     })    
-    return e
-})
+}
 
-slackEvents.on('reaction_added', e => {
-    console.log(e)
-    let params = {
-        v: 1,
-		tid: ga,
-        cid: e.user,
-        cd1: e.user,
-		cd2: e.channel,
-        cd3: '',
-        cd4: e.reaction,
-        ds: "slack",
-        cs: "slack",
-        dh: "https://temdelivery.slack.com",
-        dp:	`/${e.channel}`,
-		dt:	`Slack Channel: ${e.channel}`,
-		t: 	"event",
-        ec: 'reaction',
-        ea: `by ${e.user} in ${e.channel}`,
-        el: e.reaction,
-        ev: 1
-    }
-    request.post(`https://www.google-analytics.com/collect?${qs.stringify(params)}`, (error, response, body) => {
-        console.info(error)
-    })
-    return e
-})
+slackEvents.on('message', e => handleEvent(e))
+
+slackEvents.on('reaction_added', e => handleEvent(e))
 
 slackEvents.on('error', console.error)
 
