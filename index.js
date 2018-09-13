e7d0258d5adbd395b6617f1e10d0b4e02c560059
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-09-13 16:27:59 -0300

    moving to slackalytcs ga reports

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { createEventAdapter } = require('@slack/events-api')
 const request = require('request')
 const qs = require('querystring')
-const slackEvents = createEventAdapter('786d090b715da01bffc79e1e7ceaa52e')
+const slackEvents = createEventAdapter('0e3a482607938ee3971006e0f9768554')
 const port = process.env.PORT || 3000
 const ga = process.env.GA || 'UA-101595764-3'
 const app = express()
@@ -28,7 +28,7 @@ const handleEvent = e => {
 		t: 	"event",
         ec: e.type==="message" ? e.channel : e.item.channel,
         ea: `${e.user}`,
-        el: e.type==="message" ? e.text : e.reaction,
+        el: e.type==="message" ? `message: ${e.text}` : `reaction: ${e.reaction}`,
         ev: 1
     }
     request.post(`https://www.google-analytics.com/collect?${qs.stringify(params)}`, (error, response, body) => {
