cc8d2a9addbd998279b2742c37fd7b5b6b5a70ac
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-09-13 15:06:28 -0300

    ds

--- index.js
+++ index.js
@@ -4,25 +4,32 @@ const ua = require('universal-analytics')
 const slackEvents = createEventAdapter('786d090b715da01bffc79e1e7ceaa52e')
 const port = process.env.PORT || 3000
 const app = express()
+const visitor = ua('UA-101595764-3')
 
 app.get('/', (req, res) => res.send("i'm alive!"))
 app.use('/slack/events', slackEvents.expressMiddleware())
 
 slackEvents.on('message', e => {
     console.log(e)
+    let params = {
+        ec: 'message',
+        ea: `by ${e.user} in ${e.channel} | ${e.thread_ts ? 'is a thread reply' : ''}`,
+        el: `message`,
+        ev: `${e.text}`
+    }
+    visitor.event(params).send(r => console.info(r))
     return e
 })
 
 slackEvents.on('reaction_added', e => {
     console.log(e)
-    const visitor = ua('UA-101595764-3', e.user, {strictCidFormat: false})
     let params = {
         ec: 'reaction',
         ea: `by ${e.user} in ${e.channel}`,
         el: `reaction`,
         ev: `${e.reaction} on ${e.item.ts}`
     }
-    visitor.event(params).send()
+    visitor.event(params).send(r => console.info(r))
     return e
 })
 
