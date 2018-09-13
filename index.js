e0a987a98aa04e47dd04481ccfd97d45872ca326
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-09-13 14:12:22 -0300

    v0

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const qs = require('querystring')
 const { createEventAdapter } = require('@slack/events-api')
-const app = express()
-const signingSecret = '786d090b715da01bffc79e1e7ceaa52e'
+const slackEvents = createEventAdapter('786d090b715da01bffc79e1e7ceaa52e')
 const port = process.env.PORT || 3000
-
-const slackEvents = createEventAdapter(signingSecret)
-
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+const app = express()
 
 app.get('/', (req, res) => res.send("i'm alive!"))
-
 app.use('/slack/events', slackEvents.expressMiddleware())
 
 slackEvents.on('message', e => {
