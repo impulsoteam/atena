1b1073672409687a3e360622655d3dd5b0972978
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-09-15 19:08:27 -0300

    adding views structure, splash screen with impulso network logo, translate requests to using async/await and define ?format=json to receive json responses

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 import express from "express";
 import { createEventAdapter } from "@slack/events-api";
-import request from "request";
+import request from "async-request";
 import querystring from "querystring";
 const slackEvents = createEventAdapter("0e3a482607938ee3971006e0f9768554");
 const port = process.env.PORT || 3000;
 const slackToken = process.env.SLACK_TOKEN || "xoxp-271961991890-312238950902-435403620258-b07b50519e06e198d47d6feb92d1d5dd";
 const app = express();
 
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
-  res.header("Content-Type", "application/json");
+  if (req.query.format === "json") {
+    res.header("Content-Type", "application/json");
+  }
   next();
 });
-app.get("/", (req, res) => res.send("i'm alive!"));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Seja bem vindo! =D"
+  });
+});
+
 app.use("/slack/events", slackEvents.expressMiddleware());
 
-const handleEvent = e => {
-  console.log(e);
-  let params = {
+let response;
+
+const handleEvent = async e => {
+  const params = {
     v: 1,
     tid: process.env.GA || "UA-101595764-2",
     cid: e.user,
@@ -35,32 +47,36 @@ const handleEvent = e => {
     el: e.type === "message" ? `message: ${e.text}` : `reaction: ${e.reaction}`,
     ev: 1
   };
-  request.post(
-    `https://www.google-analytics.com/collect?${querystring.stringify(params)}`,
-    (error, response, body) => {
-      console.info(error);
-    }
-  );
+  const url = `https://www.google-analytics.com/collect?${querystring.stringify(params)}`;
+
+  try {
+    response = await request(url, { method: "POST" });
+    console.log(response.body);
+  } catch (e) {
+    consol.log(e);
+  };
 };
 
-const getUserInfo = id => {
-  request.get(
-    `https://slack.com/api/users.profile.get?token=${slackToken}&user=${id}`,
-    (error, response, body) => {
-      console.info(body);
-      return body;
-    }
-  );
+const getUserInfo = async id => {
+  const url = `https://slack.com/api/users.profile.get?token=${slackToken}&user=${id}`;
+
+  try {
+    response = await request(url);
+  } catch (e) {
+    consol.log(e);
+  };
+  return response;
 };
 
-const getChannelInfo = id => {
-  request.get(
-    `https://slack.com/api/channels.info?token=${slackToken}&channel=${id}`,
-    (error, response, body) => {
-      console.info(body);
-      return body;
-    }
-  );
+const getChannelInfo = async id => {
+  const url = `https://slack.com/api/channels.info?token=${slackToken}&channel=${id}`;
+
+  try {
+    response = await request(url);
+  } catch (e) {
+    consol.log(e);
+  };
+  return response.body;
 };
 
 slackEvents.on("message", e => handleEvent(e));
@@ -69,15 +85,26 @@ slackEvents.on("reaction_added", e => handleEvent(e));
 
 slackEvents.on("error", console.error);
 
-app.get("/slack/user/:id", (req, res) => {
-  getUserInfo(req.params.id);
-  res.send('done');
+app.get("/slack/user/:id", async (req, res) => {
+  let user = await getUserInfo(req.params.id);
+
+  res.send(user);
 });
 
-app.get("/slack/channel/:id", (req, res) => {
-  getChannelInfo(req.params.id);
-  res.send('done');
+app.get("/slack/channel/:id", async (req, res) => {
+  let channel = await getChannelInfo(req.params.id);
+  res.send(channel);
 });
 
+app.get("/ranking", (req, res) => {
+  if (req.query.format === "json") {
+    res.send({
+      impulsers: []
+    });
+  } else {
+
+    res.send("Hello World");
+  }
+});
 
 app.listen(port, () => console.info(`Listening on port ${port}`));
