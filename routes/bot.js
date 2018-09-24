05fe585359758795a78b79904f677bc8bca8f3f9
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-09-24 11:09:00 -0300

    Fix PR comments

--- routes/bot.js
+++ routes/bot.js
@@ -3,7 +3,7 @@ import express from "express";
 import userController from "../controllers/user";
 const router = express.Router();
 
-router.post("/meuspontos", async (req, res) => {
+router.post("/score", async (req, res) => {
   let user = {};
   let response = {
     text: "você não tem pontos registrados ainda =/"
@@ -25,22 +25,18 @@ router.post("/meuspontos", async (req, res) => {
 
 router.post("/ranking", async (req, res) => {
   let users = [];
-  const response = {
+  let response = {
     text: "Veja as primeiras pessoas do raking:",
     attachments: []
   };
 
   try {
     users = await userController.findAll(5);
-    if (users.length > 0) {
-      users.map((user, index) => {
-        response.attachments.push({
-          text: `${index + 1}º lugar está ${user.name} com ${
-            user.score
-          } XP, no nível ${user.level}`
-        });
-      });
-    }
+    response.attachments = users.map((user, index) => ({
+      text: `${index + 1}º lugar está ${user.name} com ${
+        user.score
+      } XP, no nível ${user.level}`
+    }));
   } catch (e) {
     console.log(e);
   }
