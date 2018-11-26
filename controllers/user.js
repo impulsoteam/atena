8c33a8b2fd6c2d7b8e29fe0372c0b332e8069b2d
Lazaro Alvarenga <lazaroalvarenga@email.com>
2018-11-26 17:05:36 -0200

    sxdsa

--- controllers/user.js
+++ controllers/user.js
@@ -159,7 +159,9 @@ const checkCoreTeam = async () => {
 const findInactivities = async () => {
   const UserModel = mongoose.model("User");
   const today = new Date();
-  const dateRange = today.setDate(today.getDate() - config.xprules.inactive.mindays);
+  const dateRange = today.setDate(
+    today.getDate() - config.xprules.inactive.mindays
+  );
   const result = await UserModel.find({
     lastUpdate: { $lt: dateRange }
   })
