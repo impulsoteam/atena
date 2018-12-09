20bec6dcc23def29f91098f6360cf1661c3ee0d1
Eduardo Junior <goldblade@gmail.com>
2018-12-09 12:28:25 -0300

    Add Cover Score with user points

--- controllers/user.test.js
+++ controllers/user.test.js
@@ -1,14 +1,14 @@
-// import request from "supertest-as-promised";
 import request from "supertest";
+import mongoose from "mongoose";
+import sinon from "sinon";
+import factory from "factory-girl";
 import crypto from "crypto";
 import qs from "qs";
-// import userController from "./user";
-// import app from "../routes/index";
 import app from "../index";
-// import { clearDataBase } from "../helpers/ClearDB";
+import User from "../models/user";
+require("sinon-mongoose");
 
 describe("[Controllers] User", () => {
-  // afterAll(() => setTimeout(() => process.exit(), 1000));
   describe("## Routes", () => {
     let MOCK_BODY = {
       token: "4l0c3fKgSeeDGqniR30aQf1O",
@@ -25,6 +25,17 @@ describe("[Controllers] User", () => {
       trigger_id: "495742047108.439439624196.324159fbe295cb6754006b3afb523a1c"
     };
 
+    let MOCK_MSG = {
+      client_msg_id: "a7cb01dd-913b-48cd-abcf-a71d603918e4",
+      type: "message",
+      text: "o/",
+      user: "UCZCQH7CG",
+      ts: "1544299021.000600",
+      channel: "CCWSMJZ6U",
+      event_ts: "1544299021.000600",
+      channel_type: "channel"
+    };
+
     let time;
     let slackSignature;
 
@@ -39,8 +50,9 @@ describe("[Controllers] User", () => {
         .digest("hex");
       slackSignature = `v0=${hmac}`;
     });
+
     describe("### Bot", () => {
-      describe("#### POST Score", () => {
+        describe("#### POST Score", () => {
         it("should return the message user has not scored points yet", done => {
           request(app)
             .post("/bot/commands/score")
@@ -55,6 +67,46 @@ describe("[Controllers] User", () => {
               expect(res.statusCode).toBe(200);
               done();
             });
+        });
+
+        it("should return the message with user position and total points", done => {
+          var user = {
+            name: "Seya",
+            level: 1,
+            score: 1,
+            slackId: "UCZCQH7CG",
+            avatar: ""
+          };
+          factory.define("User", User, user);
+
+          const UserModel = mongoose.model("User");
+
+          factory.build("User", user).then(userDocument => {
+            user = userDocument;
+            var userMock = sinon.mock(user);
+            userMock.expects("save").resolves(user);
+            sinon
+              .mock(UserModel)
+              .expects("findOne")
+              .chain("exec")
+              .resolves(user);
+
+            request(app)
+              .post("/bot/commands/score")
+              .set("x-slack-request-timestamp", time)
+              .set("x-slack-signature", slackSignature)
+              .set("Content-Type", "application/x-www-form-urlencoded")
+              .send(MOCK_BODY)
+              .then(res => {
+                expect(res.body.text).toEqual(
+                  `Olá ${user.name}, atualmente você está no nível ${
+                    user.level
+                  } com ${user.score} XP`
+                );
+                expect(res.statusCode).toBe(200);
+                done();
+              });
+          });
         });
       });
 
