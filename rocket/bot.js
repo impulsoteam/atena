caaf28bf2f0cb43278a15a6413c959779216eb82
Lázaro Alvarenga <lazaroalvarenga@email.com>
2019-01-08 10:56:20 -0200

    Update bot.js

--- rocket/bot.js
+++ rocket/bot.js
@@ -1,21 +1,20 @@
-import dotenv from "dotenv";
 import { driver } from "@rocket.chat/sdk";
 
 import interactionController from "../controllers/interaction";
 
 const runBot = async () => {
-  const conn = await driver.connect({
+  await driver.connect({
     host: process.env.ROCKET_HOST,
     useSsl: true
   });
 
-  const myuserid = await driver.login({
+  await driver.login({
     username: process.env.ROCKET_BOT_USER,
     password: process.env.ROCKET_BOT_PASS
   });
 
-  const subscribed = await driver.subscribeToMessages();
-  const msgloop = await driver.reactToMessages(processMessages);
+  await driver.subscribeToMessages();
+  await driver.reactToMessages(processMessages);
 };
 
 const processMessages = async (err, message, messageOptions) => {
