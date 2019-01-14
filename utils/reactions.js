2147058de0f6f2a092f3f935389ee1ed4e4fcfe2
Lazaro Alvarenga <lazaroalvarenga@email.com>
2019-01-14 16:18:11 -0200

    prettierfy

--- utils/reactions.js
+++ utils/reactions.js
@@ -1,11 +1,18 @@
 export const isPositiveReaction = interaction => {
-  return interaction.description === "+1" || interaction.description === ":thumbsup:";
+  return (
+    interaction.description === "+1" || interaction.description === ":thumbsup:"
+  );
 };
 
 export const isNegativeReaction = interaction => {
-  return interaction.description === "-1" || interaction.description === ":thumbsdown:";
+  return (
+    interaction.description === "-1" ||
+    interaction.description === ":thumbsdown:"
+  );
 };
 
 export const isAtenaReaction = interaction => {
-  return interaction.description === "atena" || interaction.description === ":atena:";
+  return (
+    interaction.description === "atena" || interaction.description === ":atena:"
+  );
 };
