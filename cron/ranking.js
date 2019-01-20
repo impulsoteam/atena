a23138efbbaf9950d8027245241cb31d586e6ac4
Eduardo Junior <goldblade@gmail.com>
2019-01-20 19:25:35 -0300

    add ranking crons

--- cron/ranking.js
+++ cron/ranking.js
@@ -11,4 +11,44 @@ export default async () => {
     true,
     "America/Sao_Paulo"
   );
+
+  cron.schedule(
+    "* 9 * * mon",
+    async () => {
+      await rankingController.sendToChannel();
+    },
+    null,
+    true,
+    "America/Sao_Paulo"
+  );
+
+  cron.schedule(
+    "55 23 30 4,6,9,11 *",
+    async () => {
+      await rankingController.sendToChannel();
+    },
+    null,
+    true,
+    "America/Sao_Paulo"
+  );
+
+  cron.schedule(
+    "55 23 31 1,3,5,7,8,10,12 *",
+    async () => {
+      await rankingController.sendToChannel();
+    },
+    null,
+    true,
+    "America/Sao_Paulo"
+  );
+
+  cron.schedule(
+    "55 23 28 2 *",
+    async () => {
+      await rankingController.sendToChannel();
+    },
+    null,
+    true,
+    "America/Sao_Paulo"
+  );
 };
