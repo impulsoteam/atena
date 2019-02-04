7927ef16dc7eb3a84a3df2be19c6b4bf923a1f19
Lazaro Alvarenga <lazaroalvarenga@email.com>
2019-02-03 23:42:34 -0200

    remove unnecessary imports

--- routes/slack.js
+++ routes/slack.js
@@ -3,7 +3,7 @@ import { createEventAdapter } from "@slack/events-api";
 import interactionController from "../controllers/interaction";
 import { getChannel } from "../utils/interactions";
 
-import { isValidChannel, getStyleLog, analyticsSendCollect } from "../utils";
+import { isValidChannel, getStyleLog } from "../utils";
 const router = express.Router();
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNIN_EVENTS);
 
