2a0dae34e7eb1799d7bdd6425c31451ec416446c
Lazaro Alvarenga <lazaroalvarenga@email.com>
2019-03-28 16:43:50 -0300

    atualiza estrutura do worker

--- workers/receive.js
+++ workers/receive.js
@@ -1,24 +1,32 @@
-import amqp from "amqplib";
-
-const url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+const url = process.env.CLOUDAMQP_URL;
 const queue = process.env.CLOUDMQP_QUEUE;
 
+const bail = err => {
+  console.error(err);
+  process.exit(1);
+}
+
+const consumer = conn => {
+  const ok = conn.createChannel(on_open);
+  const on_open = (err, ch) => {
+    if (err != null) bail(err);
+
+    ch.assertQueue(queue);
+    ch.consume(queue, msg => {
+      if (msg !== null) {
+        console.log(msg.content.toString());
+        ch.ack(msg);
+      }
+    });
+  }
+}
+
 const run = () => {
-  const open = amqp.connect(url);
-
-  open
-    .then(conn => {
-      let ok = conn.createChannel();
-
-      ok = ok.then(ch => {
-        ch.assertQueue(queue);
-        ch.consume(queue, msg => {
-          msg && console.log(JSON.stringify(msg));
-        });
-      });
-      return ok;
-    })
-    .then(null, console.warn);
+  require("amqplib/callback_api")
+    .connect(url, (err, conn) => {
+      if (err != null) bail(err);
+      consumer(conn);
+    });
 };
 
 run();
