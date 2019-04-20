6aefde54f7c8a377e83cbc338e77fc540bc33bf5
Thais Martins <contato@thaismartins.co>
2019-04-20 14:30:00 -0300

    Fixing save achievement level

--- utils/levels.js
+++ utils/levels.js
@@ -1,12 +1,7 @@
 import moment from "moment-timezone";
 
-import achievementLevelController from "../controllers/achievementLevel";
 import userLevelHistoryController from "../controllers/userLevelHistory";
-import {
-  getScoreToIncrease,
-  getCurrentScoreToIncrease,
-  getAchievementCurrentRating
-} from "./achievements";
+import { getScoreToIncrease, getCurrentScoreToIncrease } from "./achievements";
 import { sendEarnedAchievementMessage } from "./achievementsMessages";
 
 export const saveLevelHistoryChanges = async (userId, oldLevel, newLevel) => {
@@ -20,14 +15,6 @@ export const sendLevelMessage = async (user, achievement) => {
     range: achievement.record.range
   };
   await sendEarnedAchievementMessage(user, rating, true);
-};
-
-export const saveAchivementLevelChanges = async (
-  userId,
-  oldLevel,
-  newLevel
-) => {
-  return await achievementLevelController.save(userId, oldLevel, newLevel);
 };
 
 export const getLevelScore = achievement => {
