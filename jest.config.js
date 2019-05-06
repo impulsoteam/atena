7955cd6efc5a71656024b9a1778ecb97d356d32b
Eduardo Junior <goldblade@gmail.com>
2019-05-06 08:12:01 -0300

    add crons checkpoint

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,31 @@ module.exports = {
     // "interaction",
     "userLevelHistory",
     "user",
-    "disqus"
+    "disqus",
+    "achievementLevel",
+    "ranking",
+    "achievements",
+    "calculateReceivedScore",
+    "github"
   ],
   globalSetup: "./setup.js",
   globalTeardown: "./teardown.js",
-  testEnvironment: "./mongo-environment.js"
+  testEnvironment: "./mongo-environment.js",
+  collectCoverageFrom: [
+    "controllers/*.js",
+    "cron/*.js",
+    "helpers/*.js",
+    "models/*.js",
+    "rocket/*.js",
+    "utils/*.js",
+    "workers/*.js"
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 10,
+      branches: 10,
+      lines: 10,
+      functions: 10
+    }
+  }
 };
