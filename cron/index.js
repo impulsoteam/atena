b2dfb0d10deb8d881bab772bb1c99468aeeeb27a
Thais Martins <contato@thaismartins.co>
2019-06-19 16:40:59 -0300

    Removing refactored crons

--- cron/index.js
+++ cron/index.js
@@ -1,13 +1,9 @@
-import cronUsersInactivity from './inactivity'
-import inactivityAchievements from './inactivityAchievements'
 import cronRanking from './ranking'
 import cronWorkers from './workers'
 import cronCheckPoints from './checkpoints'
 
 export default () => {
-  cronUsersInactivity()
   cronRanking()
-  inactivityAchievements()
   cronWorkers()
   cronCheckPoints()
 }
