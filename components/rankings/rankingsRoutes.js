8b04bd8c1736ec353930b9abd1f338ac481c4c66
Thais Martins <contato@thaismartins.co>
2019-06-27 17:03:23 -0300

    Changing type to const

--- components/rankings/rankingsRoutes.js
+++ components/rankings/rankingsRoutes.js
@@ -4,17 +4,17 @@ import rankings from './rankingsController'
 const router = express.Router()
 
 router.get('/general', async (req, res) => {
-  let result = await rankings.getGeneralRanking()
+  const result = await rankings.getGeneralRanking()
   return res.json(result)
 })
 
 router.get('/monthly', async (req, res) => {
-  let result = await rankings.getRankingByMonth(req.query.month)
+  const result = await rankings.getRankingByMonth(req.query.month)
   return res.json(result)
 })
 
 router.post('/monthly', async (req, res) => {
-  let result = await rankings.generate(req.body.month)
+  const result = await rankings.generate(req.body.month)
   return res.json(result)
 })
 
