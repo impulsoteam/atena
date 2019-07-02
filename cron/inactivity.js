e7f3a1cccbf9945998c70b9ac3d3ac9913a2e519
Julio Goncalves <julio-goncalves@outlook.com>
2019-07-02 17:25:50 -0300

    fix: disable cron

--- cron/inactivity.js
+++ cron/inactivity.js
@@ -4,21 +4,19 @@ import UserController from "../controllers/user";
 import { getStyleLog } from "../utils";
 
 export default async () => {
-  cron.schedule("* * * * *", async () => {
-    let users;
-    try {
-      users = await UserController.findInactivities();
-    } catch (e) {
-      console.log(getStyleLog("red"), `\n-- error updating inactivity users`);
-      return false;
-    }
-    users.forEach(user => {
-      const score = config.xprules.inactive.value;
-      const interaction = null;
-
-      UserController.updateUserData(user, interaction, score);
-    });
-
-    return true;
+  cron.schedule("0 3 * * *", async () => {
+    // let users;
+    // try {
+    //   users = await UserController.findInactivities();
+    // } catch (e) {
+    //   console.log(getStyleLog("red"), `\n-- error updating inactivity users`);
+    //   return false;
+    // }
+    // users.forEach(user => {
+    //   const score = config.xprules.inactive.value;
+    //   const interaction = null;
+    //   UserController.updateUserData(user, interaction, score);
+    // });
+    // return true;
   });
 };
