900bf9fc9bf23e7b18d8d37e5c9dd4f15dd2aeeb
Thais Martins <contato@thaismartins.co>
2019-07-07 16:28:31 -0300

    Adding welcome message and save interaction on create user

--- components/next/nextController.js
+++ components/next/nextController.js
@@ -2,12 +2,15 @@ import service from './nextService'
 import users from '../users'
 import errors from '../errors'
 import workers from '../workers'
+import interactions from '../interactions'
 
 const file = 'Next | Controller'
 
 const handleUser = async data => {
   try {
+    let isNew = false
     let user = await service.findOrCreateUser(data)
+    if (!user.name) isNew = true
 
     user.rocketId = data.rocket_chat.id
     user.name = data.fullname
@@ -22,8 +25,17 @@ const handleUser = async data => {
 
     user = await users.save(user)
 
-    if (user.score === 0 && user.username)
+    if (isNew) {
       await users.sendWelcomeMessage(user.username)
+      await interactions.saveManual({
+        score: 0,
+        value: 0,
+        type: 'manual',
+        user: user._id,
+        username: user.username,
+        text: 'Usuário criado a partir do Next'
+      })
+    }
 
     return user
   } catch (e) {
