9b4817d321a214b5f2197400fda2b0bc8d785a62
Thais Martins <contato@thaismartins.co>
2019-08-02 13:57:22 -0300

    Feixing duplicate populate code

--- components/achievementsTemporary/achievementsTemporaryDAL.js
+++ components/achievementsTemporary/achievementsTemporaryDAL.js
@@ -1,23 +1,22 @@
 import moment from 'moment-timezone'
 import model from './achievementTemporary'
 
+const populateData = {
+  path: 'temporaryData',
+  match: { endDate: { $gte: moment(new Date()).format('YYYY-MM-DD') } }
+}
+
 const findByUser = userId => {
   return model
     .findOne({ user: userId })
-    .populate({
-      path: 'temporaryData',
-      match: { endDate: { $gte: moment(new Date()).format('YYYY-MM-DD') } }
-    })
+    .populate(populateData)
     .exec()
 }
 
 const findAllByUser = userId => {
   return model
     .find({ user: userId })
-    .populate({
-      path: 'temporaryData',
-      match: { endDate: { $gte: moment(new Date()).format('YYYY-MM-DD') } }
-    })
+    .populate(populateData)
     .exec()
 }
 
