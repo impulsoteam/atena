c128cf6fd5f5c04c47d77683c397151cba3697e9
Thais Martins <contato@thaismartins.co>
2019-08-22 23:43:03 -0300

    Fixing API auth middleware

--- middlewares/api.js
+++ middlewares/api.js
@@ -1,46 +1,69 @@
 import moment from 'moment-timezone'
 import crypto from '../components/crypto'
 import users from '../components/users'
+import errors from '../components/errors'
 
 const auth = async function(req, res, next) {
-  const authHeader = req.headers.authorization
-  const [, token] = authHeader.split(' ')
-
-  if (token) {
-    const { uuid, expireAt } = await crypto.decrypt(token)
-
-    if (!uuid) {
-      return res.sendStatus(401).json({
+  try {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+      return res.status(401).send({
+        success: false,
         type: 'invalidToken',
-        message: 'Token invalido.'
+        message: 'Nenhum token enviado.'
       })
     }
 
-    const user = await users.find({ uuid: uuid })
-    if (!user) {
-      return res.sendStatus(401).json({
+    const [, token] = authHeader.split(' ')
+
+    if (token) {
+      const { uuid, expireAt } = await crypto.decrypt(token)
+
+      if (!uuid) {
+        return res.sendStatus(401).json({
+          success: false,
+          type: 'invalidToken',
+          message: 'Token inválido.'
+        })
+      }
+
+      const user = await users.find({ uuid: uuid })
+      if (!user) {
+        return res.sendStatus(401).json({
+          success: false,
+          type: 'invalidToken',
+          message: 'Token inválido.'
+        })
+      }
+
+      if (
+        !moment(expireAt).isValid() ||
+        moment(expireAt).isBefore(moment(new Date()))
+      ) {
+        return res.status(401).json({
+          success: false,
+          type: 'sessionExpired',
+          message: 'Sua sessão expirou!'
+        })
+      }
+
+      next()
+    } else if (req.method === 'OPTIONS') {
+      next()
+    } else {
+      return res.status(401).send({
         type: 'invalidToken',
-        message: 'Token inválido.'
+        success: false,
+        message: 'Nenhum token enviado.'
       })
     }
+  } catch (e) {
+    errors._throw('API | Middleware', 'auth', e)
 
-    if (
-      !moment(expireAt).isValid() ||
-      moment(expireAt).isBefore(moment(new Date()))
-    ) {
-      return res.status(401).json({
-        type: 'sessionExpired',
-        message: 'Sua sessão expirou!'
-      })
-    }
-
-    next()
-  } else if (req.method === 'OPTIONS') {
-    next()
-  } else {
-    return res.status(401).send({
+    return res.sendStatus(401).json({
       success: false,
-      message: 'Nenhum token enviado.'
+      type: 'invalidToken',
+      message: 'Token inválido.'
     })
   }
 }
