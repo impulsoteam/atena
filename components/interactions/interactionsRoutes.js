6b9420d06e6c2cc8f558006ede1c2464f52fd3a6
Thais Martins <contato@thaismartins.co>
2019-08-26 03:58:56 -0300

    Fixing requests

--- components/interactions/interactionsRoutes.js
+++ components/interactions/interactionsRoutes.js
@@ -3,6 +3,9 @@ import interactions from './interactionsController'
 
 const router = express.Router()
 
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: false }))
+
 router.put('/users-id', async (req, res) => {
   console.log('req.body', req.body)
   const result = await interactions.changeUserId(req.body.limit, req.body.skip)
