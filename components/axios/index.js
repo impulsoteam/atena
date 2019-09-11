c9176e7dbeb1ed782e4974112fc8bf721e2cb1c8
André Cavallari <cavallari@live.com>
2019-09-11 16:08:06 -0300

    Create axios api base

---
+++ components/axios/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const sslBase =
+  process.env.ROCKETCHAT_USE_SSL === 'true' ? 'https://' : 'http://'
+const rocketchatApiUrl = `${sslBase}${process.env.ROCKETCHAT_URL}/api/v1`
+
+const onboardingApi = axios.create({
+  baseURL: rocketchatApiUrl,
+  headers: {
+    'X-User-Id': process.env.ROCKETCHAT_ONBOARDING_USER_ID,
+    'X-Auth-Token': process.env.ROCKETCHAT_USER_TOKEN
+  }
+})
+
+export default {
+  onboardingApi
+}
