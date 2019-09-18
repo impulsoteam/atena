d60ec7a39206b17c37ba611e1ef8fb8e09806c96
André Cavallari <cavallari@live.com>
2019-09-17 21:18:41 -0300

    Updates add get user info by username and invite to channels not listed

--- components/axios/adminUserApi.js
+++ components/axios/adminUserApi.js
@@ -1,36 +1,69 @@
 import axios from 'axios'
-import { apiBaseUrl } from './utils'
+import inviteUserToChannelQueue from '../queues/inviteUserToChannelQueue'
 
-const adminUserApi = axios.create({
-  baseURL: apiBaseUrl,
+const api = axios.create({
+  baseURL: `${process.env.ROCKETCHAT_URL}/api/v1`,
   headers: {
     'X-User-Id': process.env.ROCKETCHAT_ADMIN_USER_ID,
     'X-Auth-Token': process.env.ROCKETCHAT_ADMIN_USER_TOKEN
   }
 })
 
+const getUserInfoByUsername = username => {
+  return api
+    .get(`/users.info?username=${username}`)
+    .then(response => response.data.user)
+}
+
 const getChannelsList = () => {
-  return this.get('/channels.list?count=0&sort={"name":1}').then(
-    response => response.data.channels
-  )
+  return api
+    .get('/channels.list?count=0&sort={"name":1}')
+    .then(response => response.data.channels)
 }
 
 const getUserChannelsList = username => {
   const url = `/users.info?username=${username}&fields={"userRooms":1}`
-  return this.get(url).then(response => {
+  return api.get(url).then(response => {
     return response.data.user.rooms.filter(room => ['c', 'p'].includes(room.t))
   })
 }
 
 const inviteUserToChannel = (userId, roomId, type = 'c') => {
   const endpoint = type === 'c' ? '/channels.invite' : '/groups.invite'
-  return this.post(endpoint, { userId, roomId })
+  return api.post(endpoint, { userId, roomId })
 }
 
-Object.assign(adminUserApi, {
+const inviteUserToNotJoinedChannels = async username => {
+  const [user, userChannels, allChannels] = await Promise.all([
+    getUserInfoByUsername(username),
+    getUserChannelsList(username),
+    getChannelsList()
+  ])
+
+  const userChannelsNames = userChannels.map(channel => channel.name)
+  const channelsNotIn = allChannels.filter(
+    channel => !userChannelsNames.includes(channel.name)
+  )
+  const promises = channelsNotIn.map((channel, index) => {
+    return inviteUserToChannelQueue.add(
+      {
+        userId: user._id,
+        roomId: channel._id,
+        type: channel.t
+      },
+      {
+        delay: index * 60000
+      }
+    )
+  })
+
+  return Promise.all(promises)
+}
+
+export default {
+  getUserInfoByUsername,
   getChannelsList,
   getUserChannelsList,
-  inviteUserToChannel
-})
-
-export default adminUserApi
+  inviteUserToChannel,
+  inviteUserToNotJoinedChannels
+}
