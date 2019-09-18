36b1af743373006de16408ddb7e2c085bd59778f
André Cavallari <cavallari@live.com>
2019-09-17 21:16:51 -0300

    Updates * Moved onboarding API to own file * Send onboarding message before save user

--- components/axios/onboardingApi.js
+++ components/axios/onboardingApi.js
@@ -1,12 +1,23 @@
 import axios from 'axios'
-import { apiBaseUrl } from './utils'
+import fs from 'fs'
+import path from 'path'
 
-const onboardingApi = axios.create({
-  baseURL: apiBaseUrl,
+const onboardingTextPath = path.join(process.cwd(), 'config', 'onboarding.txt')
+const text = fs.readFileSync(onboardingTextPath).toString()
+
+const api = axios.create({
+  baseURL: `${process.env.ROCKETCHAT_URL}/api/v1`,
   headers: {
     'X-User-Id': process.env.ROCKETCHAT_ONBOARDING_USER_ID,
     'X-Auth-Token': process.env.ROCKETCHAT_ONBOARDING_USER_TOKEN
   }
 })
 
-export default onboardingApi
+const sendOnboardingMessage = username => {
+  const payload = { channel: `@${username}`, text }
+  return api.post('/chat.postMessage', payload)
+}
+
+export default {
+  sendOnboardingMessage
+}
--- components/axios/utils.js
+++
@@ -1,9 +0,0 @@
-const apiBaseUrl = (() => {
-  const useSsl = process.env.ROCKETCHAT_USE_SSL === 'true'
-  const httpBase = useSsl ? 'https://' : 'http://'
-  return `${httpBase}${process.env.ROCKETCHAT_URL}/api/v1`
-})()
-
-export default {
-  apiBaseUrl
-}
