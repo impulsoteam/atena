fa5e59a1700458a4aade1bd91198525fad129960
André Cavallari <cavallari@live.com>
2019-10-03 11:43:42 -0300

    Create message handlers

---
+++ components/rocket/messagesHandlers.js
@@ -0,0 +1,84 @@
+import User from '../users/user'
+import Message from '../models/message'
+import Reaction from '../models/reaction'
+import Score from '../models/score'
+import config from 'config-yml'
+import commands from '../commands'
+import commandUtils from '../commands/commandsUtils'
+
+export default async message => {
+  const msg = await Message.findOne({ 'rocketData.messageId': message._id })
+  const user = await User.findOne({ rocketId: message.u._id })
+
+  if (isCommand(message)) return handleCommand(message, user)
+  if (!msg && !message.tmid) return handleNewMessage(message, user)
+  if (!msg && message.tmid) return handleReply(message, user)
+  return handleReactions(message, user, msg)
+}
+
+const isCommand = message => {
+  const commandsList = Object.values(commandUtils.getCommandsRegex())
+  return commandsList.find(command => new RegExp(command).test(message.msg))
+}
+
+const handleCommand = (message, user) => {
+  commands.handle(message)
+  Message.create({
+    user: user._id,
+    'rocketData.messageId': message._id,
+    'rocketData.roomId': message.rid,
+    content: message.msg,
+    'is.command': true
+  })
+}
+
+const handleNewMessage = async (message, user) => {
+  const msg = await Message.create({
+    user: user._id,
+    'rocketData.messageId': message._id,
+    'rocketData.roomId': message.rid,
+    content: message.msg
+  })
+
+  Score.create({
+    value: config.xprules.messages.send,
+    description: 'New message',
+    ref: msg._id,
+    refModel: 'Message'
+  })
+}
+
+const handleReply = async (message, user) => {
+  const parentMessage = await Message.findOne({
+    'rocketData.messageId': message.tmid
+  })
+  const msg = await Message.create({
+    user: user._id,
+    'rocketData.messageId': message._id,
+    'rocketData.roomId': message.rid,
+    'rocketData.parent': message.tmid,
+    content: message.msg,
+    parent: parentMessage._id
+  })
+
+  Score.create({
+    value: config.xprules.threads.send,
+    description: 'New reply sent',
+    ref: msg._id,
+    refModel: 'Message'
+  })
+
+  Score.create({
+    value: config.xprules.threads.receive,
+    description: 'New reply received',
+    ref: parentMessage._id,
+    refModel: 'Message'
+  })
+
+  parentMessage.is.thread = true
+  parentMessage.save()
+}
+
+const handleReactions = async (message, user, msg) => {
+  // Handle the reactions
+}
