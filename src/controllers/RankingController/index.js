d927870a0ffe526391cdb7ebf2bf3eebb97db489
Julio Goncalves <julio-goncalves@outlook.com>
2020-05-12 12:57:27 -0300

    chore: remove _id in achievements

--- src/controllers/RankingController/index.js
+++ src/controllers/RankingController/index.js
@@ -105,3 +105,47 @@ class RankingController extends RankingUtils {
 }
 
 export default new RankingController()
+const a = {
+  impulser: {
+    fullname: 'Leonardo',
+    email: 'talkto@theleoad.com',
+    network_email: 'leonardo@impulser.me',
+    secondary_email: null,
+    phone: '629991222121',
+    telegram: null,
+    photo:
+      'https://lh3.googleusercontent.com/a-/AOh14GgYGTHVgTzL4z9S3350vKRMg0al2pMSfKFyMRQ3Ag',
+    contact_preferences: ['email'],
+    onboarding_step: 'recently_signedin',
+    self_titled_seniority: 'pleno',
+    stacks: ['Apache'],
+    main_stacks: ['Apache'],
+    address_street: 'Avenida Federal',
+    address_number: '',
+    address_zipcode: '75080045',
+    address_neighbourhood: 'Maracananzinho',
+    address_complement: '',
+    address_city: 'Anápolis',
+    address_state: 'Goiás',
+    address_country: 'BR',
+    address_confirmed: true,
+    public_uuid: '97b45400-4510-4146-a9dd-0fd4ce0904a4',
+    referrer_link: 'https://staging.impulser.me/3ef21c16',
+    gender: null,
+    race_color: null,
+    available_to_opportunities: true,
+    job_title: null,
+    summary_by_impulser: null,
+    years_of_experience: null,
+    need_financial_details: false,
+    onboarding_complete: true,
+    clube25f_access: false,
+    username: 'leonardo',
+    network_email_smtp_password: 'asdf123',
+    network_email_status: 'created',
+    rocket_chat_user_id: 'zDXMrYiAm4Gy7TPfk',
+    rocket_chat_deep_link: true,
+    rocket_chat_token: 'fSiIWMlaK_5vqmaX6-nxoVBvCC2RwK__sal-dJn2iUs',
+    rocket_chat_token_expires_at: '2020-05-15T18:11:56.956-03:00'
+  }
+}
