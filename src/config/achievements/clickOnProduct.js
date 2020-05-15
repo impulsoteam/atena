154b9393cdae9b5c8d734afa4a0526a57a8723c5
Julio Goncalves <julio-goncalves@outlook.com>
2020-05-15 12:58:03 -0300

    chore: products achievements

--- src/config/achievements/clickOnProduct.js
+++ src/config/achievements/clickOnProduct.js
@@ -49,7 +49,7 @@ export const clickOnProduct = name => {
   const [product] = Object.entries(products).find(([, type]) => type === name)
 
   return {
-    displayAchievement: `Clique em Produtos - ${product}`,
+    displayAchievement: `Clique em ${product}`,
     medals,
     name
   }
