125fbaaa49ba32150d2521e2b43da69cb1daddc6
Julio Goncalves <julio-goncalves@outlook.com>
2020-05-19 17:05:35 -0300

    chore: remove comments

--- src/config/achievements/messageSended.js
+++ src/config/achievements/messageSended.js
@@ -1,74 +1,33 @@
-// export const messageSended = () => {
-//   const medals = [
-//     {
-//       name: 'bronze',
-//       displayMedal: 'Bronze',
-//       targets: [1, 2, 3, 5, 8],
-//       score: 5
-//     },
-//     {
-//       name: 'silver',
-//       displayMedal: 'Prata',
-//       targets: [16, 27, 40, 56, 75],
-//       score: 10
-//     },
-//     {
-//       name: 'gold',
-//       displayMedal: 'Ouro',
-//       targets: [108, 147, 192, 243, 300],
-//       score: 15
-//     },
-//     {
-//       name: 'platinum',
-//       displayMedal: 'Platina',
-//       targets: [385, 481, 588, 705, 833],
-//       score: 20
-//     },
-//     {
-//       name: 'diamond',
-//       displayMedal: 'Diamante',
-//       targets: [1008, 1200, 1408, 1633, 1875, null],
-//       score: 25
-//     }
-//   ]
-
-//   return {
-//     displayAchievement: 'Mensagens Enviadas',
-//     name: 'messageSended',
-//     medals
-//   }
-// }
-
 export const messageSended = () => {
   const medals = [
     {
       name: 'bronze',
       displayMedal: 'Bronze',
-      targets: [1, 2, 3, 4, 5],
+      targets: [1, 2, 3, 5, 8],
       score: 5
     },
     {
       name: 'silver',
       displayMedal: 'Prata',
-      targets: [6, 7, 8, 9, 10],
+      targets: [16, 27, 40, 56, 75],
       score: 10
     },
     {
       name: 'gold',
       displayMedal: 'Ouro',
-      targets: [11, 12, 13, 14, 15],
+      targets: [108, 147, 192, 243, 300],
       score: 15
     },
     {
       name: 'platinum',
       displayMedal: 'Platina',
-      targets: [16, 17, 18, 19, 20],
+      targets: [385, 481, 588, 705, 833],
       score: 20
     },
     {
       name: 'diamond',
       displayMedal: 'Diamante',
-      targets: [21, 22, 23, 24, 26, null],
+      targets: [1008, 1200, 1408, 1633, 1875, null],
       score: 25
     }
   ]
