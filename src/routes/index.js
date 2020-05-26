fab5b2c7ff60ea399642a2a133ae24c266439f7e
Julio Goncalves <julio-goncalves@outlook.com>
2020-05-26 17:03:25 -0300

    chore: remove atena-frontend

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import { Router } from 'express'
 
 import apiRoutes from './api'
-import atenaRoutes from './atena'
 
 const router = new Router()
 
 router.use('/api/v1', apiRoutes)
-router.use('/api/v2', atenaRoutes)
 
 export default router
