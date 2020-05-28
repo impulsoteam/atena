b35c34c9b8a55f9a77dd445aa3fa4f0346a17323
Julio Goncalves <julio-goncalves@outlook.com>
2020-05-28 15:48:38 -0300

    chore: drip route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,8 @@ import atenaRoutes from './atena'
 import dripRoutes from './drip'
 
 const router = new Router()
-
 router.use('/api/v1', apiRoutes)
-router.use('/drip', dripRoutes)
+router.use('/api/v1/drip', dripRoutes)
 router.use('/api/v2', atenaRoutes)
 
 export default router
