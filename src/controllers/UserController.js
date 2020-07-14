82829454d93a5d70bb3bf7f7446eabdcfdaab2ce
Julio Goncalves <julio-goncalves@outlook.com>
2020-07-13 21:33:11 -0300

    chore: add drip custom fields

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,8 @@
 import moment from 'moment'
 
 import { onboardingMessage } from '../assets/onboarding'
+import { achievementTypes } from '../config/achievements'
+import { levels } from '../config/score'
 import User from '../models/User'
 import { sendBatchOfUsersToDrip } from '../services/drip'
 import { sleep } from '../utils'
@@ -91,6 +93,9 @@ class UserController {
 
         const customFields = {
           atena_level: level.value,
+          score_to_next_level:
+            level.scoreToNextLevel || levels[0].scoreToNextLevel,
+          number_of_achievements: achievementTypes.length,
           ranking_monthly_position: parseInt(position) + 1,
           ranking_monthly_score: user.score,
           ranking_general_position:
