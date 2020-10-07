d94c01ca5576914054e97172be38f8c2b4f7bc3e
Julio Goncalves <julio-goncalves@outlook.com>
2020-10-07 02:53:14 -0300

    chrore: add package

--- src/services/amqp/miner.js
+++ src/services/amqp/miner.js
@@ -11,7 +11,7 @@ export const handle = async ({ message, channel }) => {
     const types = {
       emailOpened: handleEmailOpenedInteraction
     }
-
+    console.log(data)
     const service = types[properties.type]
 
     if (service) await service(data)
