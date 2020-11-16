0b3e3f2a3d48d19d71b8fb855d416b0e6817d505
Julio Goncalves <julio-goncalves@outlook.com>
2020-11-16 13:00:33 -0300

    fix: message conditional

--- src/services/rocketchat/driver.js
+++ src/services/rocketchat/driver.js
@@ -10,9 +10,10 @@ export const connect = async () => {
     const botId = await driver.login()
 
     await driver.subscribeToMessages()
-    driver.reactToMessages((error, message, messageOptions) => {
+    driver.reactToMessages((error, messages, messageOptions) => {
       if (error) throw new Error('Error in driver.reactToMessages')
 
+      const message = messages[0] || messages
       if ((message.u && message.u._id === botId) || message.t) return
 
       handlePayload({ message, messageOptions })
