6a5246c366f7175e068a28ead6a79a5b0e80408f
Julio Goncalves <julio-goncalves@outlook.com>
2021-02-05 18:18:41 -0300

    fix: change schedule lib

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -8,7 +8,7 @@ import { inviteUserToNotJoinedChannels } from '../services/rocketchat/api'
 export const scheduleJobs = () => {
   scheduleJob('0 0 * * *', inviteUserToNotJoinedChannels)
   scheduleJob('0 1 * * *', UserController.updateEmailServices)
-  scheduleJob('0 0 1 * * *', RankingController.resetMonthlyRanking)
+  scheduleJob('0 0 1 * *', RankingController.resetMonthlyRanking)
   scheduleJob('30 13 * * mon', BotController.sendMonthlyRankingToChannel)
   scheduleJob('00,20,40 * * * *', RankingController.createMonthlyRanking)
   scheduleJob('10,30,50 * * * *', RankingController.createGeneralRanking)
