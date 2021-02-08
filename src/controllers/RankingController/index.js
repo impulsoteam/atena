ae7a69cd57382c18aa5296ded23595f1aab5e8a6
Julio Goncalves <31070014+thejullius@users.noreply.github.com>
2021-02-08 19:36:36 -0300

    fix: change schedule lib (#735)
    * fix: change schedule lib
    
    * fix: change schedule lib
    
    * fix: change schedule lib
    
    * fix: change schedule lib
    
    * chore: add async
    
    * chore: change time
    
    * chore: change sort order
    
    * chore: chang
    
    * chore: add index
    
    * chore: add while
    
    * chore: add while

--- src/controllers/RankingController/index.js
+++ src/controllers/RankingController/index.js
@@ -1,3 +1,4 @@
+import { sendError } from 'log-on-slack'
 import moment from 'moment'
 
 import GeneralRanking from '../../models/GeneralRanking'
@@ -30,10 +31,17 @@ class RankingController extends RankingUtils {
   }
 
   async createMonthlyRanking() {
-    const { ranking } = await Score.findAllByMonth({ offset: 0, size: 99999 })
+    try {
+      const { ranking } = await Score.findAllByMonth({ offset: 0, size: 99999 })
 
-    for (const [index, user] of ranking.entries()) {
-      await MonthlyRanking.updateUserRanking({ user, position: index + 1 })
+      for (const [index, user] of ranking.entries()) {
+        await MonthlyRanking.updateUserRanking({ user, position: index + 1 })
+      }
+    } catch (error) {
+      sendError({
+        file: 'RankingController.createMonthlyRanking',
+        error
+      })
     }
   }
 
@@ -42,29 +50,46 @@ class RankingController extends RankingUtils {
   }
 
   async createGeneralRanking() {
-    const ranking = await User.aggregate([
-      {
-        $match: {
-          isCoreTeam: false,
-          'score.value': { $gt: 0 }
-        }
-      },
-      { $sort: { 'score.value': -1 } },
-      {
-        $project: {
-          _id: 0,
-          rocketchat: 1,
-          name: 1,
-          avatar: 1,
-          score: '$score.value',
-          level: '$level.value',
-          uuid: 1
-        }
-      }
-    ])
+    try {
+      const limit = 10000
+      let skip = 0
+      let position = 1
 
-    for (const [index, user] of ranking.entries()) {
-      await GeneralRanking.updateUserRanking({ user, position: index + 1 })
+      while (true) {
+        const ranking = await User.find(
+          {
+            isCoreTeam: false,
+            'score.value': { $gt: 0 }
+          },
+          {
+            _id: 0,
+            rocketchat: 1,
+            name: 1,
+            avatar: 1,
+            score: '$score.value',
+            level: '$level.value',
+            uuid: 1
+          }
+        )
+          .sort({
+            'score.value': -1
+          })
+          .limit(limit)
+          .skip(skip)
+
+        if (!ranking.length) break
+
+        for (const user of ranking) {
+          await GeneralRanking.updateUserRanking({ user, position })
+          position++
+        }
+        skip += limit
+      }
+    } catch (error) {
+      sendError({
+        file: 'RankingController.createGeneralRanking',
+        error
+      })
     }
   }
 }
--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,47 +1,15 @@
-import cron from 'node-cron'
+import { scheduleJob } from 'node-schedule'
 
 import BotController from '../controllers/BotController'
 import RankingController from '../controllers/RankingController'
 import UserController from '../controllers/UserController'
 import { inviteUserToNotJoinedChannels } from '../services/rocketchat/api'
 
-const sendMonthlyRankingToChannel = () => {
-  cron.schedule('30 13 * * mon', () =>
-    BotController.sendMonthlyRankingToChannel()
-  )
-}
-
-const inviteUsersToChannel = () => {
-  cron.schedule('0 0 * * *', () => inviteUserToNotJoinedChannels())
-}
-
-const updateEmailServices = () => {
-  cron.schedule('0 1 * * *', () => UserController.updateEmailServices())
-}
-
-const resetMonthlyRanking = () => {
-  cron.schedule('0 0 1 * * *', () => RankingController.resetMonthlyRanking())
-}
-
-const updateMonthlyRanking = () => {
-  cron.schedule('00,20,40 * * * *', () =>
-    RankingController.createMonthlyRanking()
-  )
-}
-
-const updateGeneralRanking = () => {
-  cron.schedule('10,30,50 * * * *', () =>
-    RankingController.createGeneralRanking()
-  )
-}
-
-export const exec = () => {
-  sendMonthlyRankingToChannel()
-  inviteUsersToChannel()
-  updateEmailServices()
-  resetMonthlyRanking()
-  updateMonthlyRanking()
-  updateGeneralRanking()
-  inviteUsersToChannel()
-  sendMonthlyRankingToChannel()
+export const scheduleJobs = () => {
+  scheduleJob('0 0 * * *', inviteUserToNotJoinedChannels)
+  scheduleJob('0 1 * * *', UserController.updateEmailServices)
+  scheduleJob('0 0 1 * *', RankingController.resetMonthlyRanking)
+  scheduleJob('30 13 * * mon', BotController.sendMonthlyRankingToChannel)
+  scheduleJob('00,20,40 * * * *', RankingController.createMonthlyRanking)
+  scheduleJob('10,30,50 * * * *', RankingController.createGeneralRanking)
 }
