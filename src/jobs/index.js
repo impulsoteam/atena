918fcd1f927e4ec8b55401b1bd21b29af3a23f8d
Mariana Viana <immarianaviana@gmail.com>
2021-08-13 09:43:16 -0300

    remove rocketchat jobs

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,15 +1,11 @@
 import { scheduleJob } from 'node-schedule'
 
-import BotController from '../controllers/BotController'
 import RankingController from '../controllers/RankingController'
 import UserController from '../controllers/UserController'
-import { inviteUserToNotJoinedChannels } from '../services/rocketchat/api'
 
 export const scheduleJobs = () => {
-  scheduleJob('0 0 * * *', inviteUserToNotJoinedChannels)
   scheduleJob('0 1 * * *', UserController.updateEmailServices)
   scheduleJob('0 0 1 * *', RankingController.resetMonthlyRanking)
-  scheduleJob('30 13 * * mon', BotController.sendMonthlyRankingToChannel)
   scheduleJob('00,20,40 * * * *', RankingController.createMonthlyRanking)
   scheduleJob('10,30,50 * * * *', RankingController.createGeneralRanking)
 }
